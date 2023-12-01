kalmans prediction without camera shows rotation in correct direction

filtering/KalmanFilterLibrary.py
@@ -39,10 +39,12 @@ class ThymioKalmanFilter:
             would cause a large change in the angle and break the smooth prediction).
     """
 
-    def __init__(self, position_thymio_camera_est, direction_thymio_camera_est):
+    def __init__(self, position_thymio_camera_est, direction_thymio_camera_est, max_map_width, max_map_height):
         self.kf = None
         self.last_temp_direction_angle_camera = None
         self.total_direction_angle_camera = None
+        self.max_map_width = max_map_width
+        self.max_map_height = max_map_height
 
         self._initialize_kalman_filter(position_thymio_camera_est, direction_thymio_camera_est)
 
@@ -100,6 +102,7 @@ class ThymioKalmanFilter:
         :return: None
         """
 
+        # -100 <= v <= 100
         v = (left_wheel_speed + right_wheel_speed) / 2.0
 
         right_wheel_speed = max(min(right_wheel_speed, 100), -100)
@@ -111,17 +114,20 @@ class ThymioKalmanFilter:
                     right_wheel_speed - left_wheel_speed)) * MIN_THYMIO_360_TURN_PERIOD  # positive sense is counterclockwise
         w = 0
         if period != 0:
-            w = 2 * np.pi / period
+            w = - 2 * np.pi / period
 
         # TODO: self.kf.F always only can contain integers --> rounded down --> |w| is almost always < 1
-        self.kf.F[2, 3] = w  # * 0.1 #DT  # becomes theta_k+1 = theta_k + w * DT
+        self.kf.F[2, 3] = w * 0.1  # * 0.1 #DT  # becomes theta_k+1 = theta_k + w * DT
 
-        if abs(v) < 20:
+        if abs(v) < 5:
             # Pure rotation without translation, i.e., the thymio is not moving forward or backward
             # --> keep the postion the same
-            self.kf.F[0, 3] = 1  # 0.0001 # 1
-            self.kf.F[1, 3] = 1  # 0.0001 # 1
+            self.kf.F[0, 3] = 0 #1  # 0.0001 # 1
+            self.kf.F[1, 3] = 0 #1  # 0.0001 # 1
         else:
+            v = v * 0.2
+            print("v", v)
+
             # Translation with rotation
             print("v * math.cos(self.kf.x[2]) * DT", v * math.cos(self.kf.x[2]) * DT)
             self.kf.F[0, 3] = v * math.cos(self.kf.x[2]) * DT  # * 0.0001  # becomes x_k+1 = x_k + v * cos(theta) * DT
@@ -169,7 +175,12 @@ class ThymioKalmanFilter:
         Get the estimated location of the Thymio.
         :return: tuple of ints (x, y)
         """
-        return int(self.kf.x[0]), int(self.kf.x[1])
+        est_x = int(self.kf.x[0])
+        est_y = int(self.kf.x[1])
+
+        est_x = min(max(est_x, 0), self.max_map_width - 1)
+        est_y = min(max(est_y, 0), self.max_map_height - 1)
+        return est_x, est_y
 
     def get_angle_rad_est(self):
         """

map/GridMap.py
@@ -97,7 +97,7 @@ class GridMap:
         x, y = self._convert_to_centroid_grid_indices(corners_for_thymio, len(binary_image[0]), len(binary_image))
         direction = corners_for_thymio[0] - corners_for_thymio[3]
 
-        self.kalman_filter = ThymioKalmanFilter(np.array([x, y]), direction)
+        self.kalman_filter = ThymioKalmanFilter(np.array([x, y]), direction, max_map_width=width, max_map_height=height)
         self._thymio_kalman_location = np.array([x, y])
         self._prev_thymio_kalman_location = np.array([x, y])
 
@@ -440,17 +440,36 @@ class GridMap:
         y = int(centroid[1] / video_feed_height * self._height)
         return x, y
 
-    def display_grid_as_image(self):
+    def display_grid_as_image(self, direction_change_idx=None):
         """
         Displays the current grid as an image
         :return: None
         """
-        if self._grid_image_is_up_to_date:
-            cv2.imshow("grid map", self.grid_image)
-        else:
+        image = self.grid_image
+        if not self._grid_image_is_up_to_date:
             self._compute_grid_image()
-            cv2.imshow("grid map", self.grid_image)
+            # cv2.imshow("grid map", self.grid_image)
+            image = self.grid_image
             self._grid_image_is_up_to_date = True
+
+        # draw wanted and actual directions
+        if direction_change_idx is not None and direction_change_idx < len(self.direction_changes):
+            thymio_location = np.array(self.kalman_filter.get_location_est())
+            thymio_direction = tuple(map(int, 10 * np.array(self.kalman_filter.get_direction_est())))
+            wanted_direction = tuple(map(int, (np.array(self.direction_changes[direction_change_idx]) - thymio_location)))
+
+            print("thymio_location", thymio_location)
+            print("thymio_direction", thymio_direction)
+            print("wanted_direction", wanted_direction)
+
+            image = cv2.arrowedLine(image, tuple(thymio_location),
+                                  tuple(np.add(thymio_location, 10 * np.array(thymio_direction))), (255, 0, 0), 2)
+
+            image = cv2.arrowedLine(image, tuple(thymio_location),
+                                  tuple(np.add(thymio_location, 10 * np.array(wanted_direction))), (0, 0, 0), 2)
+
+
+        cv2.imshow("grid map", image)
 
         # thymio_location = self.get_thymio_grid_coordinates()
         # goal_location = self.get_goal_grid_coordinates()
@@ -576,7 +595,7 @@ class GridMap:
                                            np.array(self._thymio_kalman_location) + np.array(
                                                tuple(map(int, 100 * np.array(self._thymio_kalman_direction)))),
                                            (255, 0, 0), 2) # this is color (BGR)
-            cv2.imshow("image with thymio direction", temp_dir_img)
+            # cv2.imshow("image with thymio direction", temp_dir_img)
 
         # direction = self._thymio_corners[0][0] - self._thymio_corners[0][3]
         # normalized_direction = direction / np.linalg.norm(direction)